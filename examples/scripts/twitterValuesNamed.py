606b12d6bdfe8841b84c895388f984dea065905a
Cefn Hoile <github.com@cefn.com>
2017-08-08 00:18:54 +0100

    Example searching for multiple keys

---
+++ examples/scripts/twitterValuesNamed.py
@@ -0,0 +1,32 @@
+from medea import Tokenizer
+from examples.scripts import timeit
+
+def generateTweets():
+    source = open('examples/data/trumpTweet.json')
+    tokenizer = Tokenizer(source)
+
+    ids = []
+    texts = []
+
+    def generatorFactory(name):
+        if name == "id":
+            for tok, val in tokenizer.tokenizeValue():
+                ids.append(val)
+        elif name == "text":
+            for tok, val in tokenizer.tokenizeValue():
+                texts.append(val)
+        elif name == "user":
+            pass
+        yield from ()
+
+    # yield forces the generator factories to be invoked until stream empty, but actually generates ()
+    yield from tokenizer.generateFromNamed(["id","text","user"], generatorFactory)
+
+    # this is doing the real yielding
+    yield from zip(ids, texts)
+
+def run():
+    for id, text in generateTweets():
+        print("{} : '{}'".format(id, text))
+
+timeit(run)
