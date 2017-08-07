892c00e1f87d3446486d1aee257b7835c91b1df0
Cefn Hoile <github.com@cefn.com>
2017-08-08 00:18:28 +0100

    Now split for a twitter and a weather example

--- examples/scripts/valuesNamed.py
+++
@@ -1,9 +0,0 @@
-from machine import freq
-freq(160000000)
-from medea import Tokenizer
-
-source = open('examples/data/weathermap.json')
-tokenizer = Tokenizer(source)
-
-for tok, val in tokenizer.tokenizeValuesNamed("rain"):
-    print(tok, val)
