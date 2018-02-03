02705c8e6b4f2668abf0ff2f6e3bf8965bbe4ad0
Cefn Hoile <github.com@cefn.com>
2018-02-03 10:59:42 +0000

    Consistent naming

--- medea/file.py
+++ medea/file.py
@@ -1,12 +1,12 @@
 from medea import defaultBufferSize
 
 
-def createFileStreamGenerator(path, buffer=None):
+def createFileByteGeneratorFactory(path, buffer=None):
     """
     Invoke with next(gen) or gen.send(True) to consume byte from stream (read AND increment)
     Invoke with gen.send(False) to peek at byte (read WITHOUT incrementing)
     """
-    def fileStreamGenerator():
+    def byteGeneratorFactory():
         nonlocal buffer
         if buffer is None:
             buffer = bytearray(defaultBufferSize)
@@ -18,11 +18,9 @@ def createFileStreamGenerator(path, buffer=None):
                 if count > 0:
                     while pos < count:
                         nextByte = buffer[pos]
-                        if consumed:
+                        if consumed is not False:
                             pos += 1
                         consumed = yield nextByte
-                        if consumed is None:
-                            consumed = True
                 else:
                     break
-    return fileStreamGenerator
+    return byteGeneratorFactory
