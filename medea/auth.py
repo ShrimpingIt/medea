36f039e99e5323c1d951057da9642e8d72d8a086
Cefn Hoile <github.com@cefn.com>
2018-02-03 10:57:57 +0000

    Tidied

--- medea/auth.py
+++ medea/auth.py
@@ -1,4 +1,3 @@
-wifiName = "SkyHome"
-wifiPass = "c3fnh0ile"
-twitterBearerId =     b'AAAAAAAAAAAAAAAAAAAAAJKj1gAAAAAAS7Lo%2BpzmCRW%2FqpZjN2yOzmZyKVE%3DHBc42VkI2zpJ2v3z7zcwVErFQqG1IJT1LbFvXpMmwNg7B3fhDY'
-
+wifiName = ""
+wifiPass = ""
+twitterBearerId = b''
