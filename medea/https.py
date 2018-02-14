b9dadf5859598e10e0a5fa18053939dcbd7d6618
Cefn Hoile <github.com@cefn.com>
2018-02-14 17:16:09 +0000

    Fixed https contentLength-based ssl socket buffer hang using generator send to signal remaining content bytes. Removed explicit socket close call.

--- medea/https.py
+++ medea/https.py
@@ -2,13 +2,17 @@ import sys
 from medea import defaultBufferSize
 from medea.agnostic import socket, ssl, SocketTimeoutError
 
-def createHttpsByteGeneratorFactory(url, headers=None, timeout=1.0, buffer=None):
+def createHttpsByteGeneratorFactory(url, headers=None, timeout=1.0, buffer=None, bufferSize=defaultBufferSize):
     def byteGeneratorFactory():
         _, _, host, path = url.split('/', 3)
-        nonlocal buffer
+        nonlocal buffer, bufferSize
         if buffer is None:
-            buffer = bytearray(defaultBufferSize)
-        addr = socket.getaddrinfo(host, 443)[0][-1]
+            buffer = bytearray(bufferSize)
+        bufferSize = len(buffer)
+        try:
+            addr = socket.getaddrinfo(host, 443)[0][-1]
+        except IndexError:
+            raise Exception("Not connected to Wifi")
         s = socket.socket()
         s.connect(addr)
         if hasattr(s, 'settimeout'):
@@ -26,20 +30,35 @@ def createHttpsByteGeneratorFactory(url, headers=None, timeout=1.0, buffer=None)
             s.write(b'\r\n')
             consumed = False
 
+            remaining = None
+
             while True:
                 if sys.implementation.name == "micropython":
-                    count = s.readinto(buffer)
+                    if remaining is None or bufferSize < remaining:
+                        count = s.readinto(buffer)
+                    else: # read into memoryview shorter than backing buffer
+                        count = s.readinto(memoryview(buffer)[:remaining])
                 else:
-                    count = s.read(len(buffer), buffer)
+                    if remaining is None or bufferSize < remaining:
+                        count = s.read(bufferSize, buffer)
+                    else:
+                        count = s.read(remaining, memoryview(buffer)[:remaining])
                 pos = 0
                 if count > 0:
                     while pos < count:
                         nextByte = buffer[pos]
                         if consumed:
                             pos += 1
+                            if remaining is not None:
+                                remaining -= 1
                         consumed = yield nextByte
                         if consumed is None:
                             consumed = True
+                        elif consumed is False:
+                            pass
+                        elif type(consumed) is int: # allow signalling content length
+                            remaining = consumed
+                            consumed = False
                 else:
                     break
         except StopIteration:
@@ -52,8 +71,10 @@ def createHttpsByteGeneratorFactory(url, headers=None, timeout=1.0, buffer=None)
             print("Unexpected exception")
             print(e)
             raise
+        """
         finally:
             s.close()
+        """
     return byteGeneratorFactory
 
 
@@ -100,8 +121,10 @@ def createHttpsContentByteGeneratorFactory(*a, **k):
     which stops iteration after the proper count of content bytes"""
     upstreamGeneratorFactory = createHttpsByteGeneratorFactory(*a, **k)
     def byteGeneratorFactory():
+        """Yield+send delegation Based on answer shared at https://stackoverflow.com/questions/48584098/yield-based-equivalent-to-python3-yield-from-delegation-without-losing-send"""
         byteGenerator = upstreamGeneratorFactory()
         contentLength = processHttpHeaders(byteGenerator)
+        byteGenerator.send(contentLength) # signal byte count remaining
         try:
             value = next(byteGenerator)
         except StopIteration:
