a981d22f11578b49a7d11ed31a7f8d526f6242a2
Cefn Hoile <github.com@cefn.com>
2018-02-14 12:38:07 +0000

    Duplicating since main example now uses live Twitter

---
+++ examples/scripts/trumpTweetFile.py
@@ -0,0 +1,28 @@
+from medea import visit
+from medea.https import createHttpsContentByteGeneratorFactory
+from medea.file import createFileByteGeneratorFactory
+from medea.twitter import twitterHeaders, createTwitterTimelineUrl
+
+filename = "trump.content"
+
+def visitor(tok, val):
+    print(tok, val)
+
+def writefile():
+    twitterUrl = createTwitterTimelineUrl('realDonaldTrump', count=1)
+    streamGenerator = createHttpsContentByteGeneratorFactory(twitterUrl, twitterHeaders)
+    stream = streamGenerator()
+    with open(filename, "wb") as f:
+        try:
+            while True:
+                f.write(chr(next(stream)).encode('ascii'))
+        except StopIteration:
+            pass
+
+def readfile():
+    streamGenerator = createFileByteGeneratorFactory(filename)
+    visit(streamGenerator, visitor)
+
+
+#writefile()
+readfile()
