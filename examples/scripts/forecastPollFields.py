5d0d12c9e25965c26c06c7a3d223ab4aa80f05a0
Cefn Hoile <github.com@cefn.com>
2018-02-15 15:13:32 +0000

    Long-lived polling example of weather forecast

---
+++ examples/scripts/forecastPollFields.py
@@ -0,0 +1,19 @@
+import gc
+import sys
+from time import sleep
+from examples.scripts.forecastExtractFields import run
+
+def loop():
+    if sys.platform == "esp8266" or sys.platform == "esp32":
+        from medea.auth import wifiName, wifiPass
+        from medea.wifi import connect
+        assert wifiName is not None and wifiPass is not None, "Provide wifi credentials"
+        connect(wifiName.decode('utf8'), wifiPass.decode('utf8'))
+
+    while True:
+        run()
+        gc.collect()
+        sleep(120)
+
+if __name__ == "__main__":
+    loop()
