6d45ddcd34618e24552e41b26941688538cf7c3a
Cefn Hoile <github.com@cefn.com>
2018-02-15 14:16:25 +0000

    No longer removing auth. Can be interactively overridden

--- put.sh
+++ put.sh
@@ -4,7 +4,7 @@ PORT=/dev/ttyUSB0
 # filter directories, and create relevant ones on board
 find . -mindepth 1 -type d | grep -v -E "(.(git|idea|vscode)|__pycache__)" | xargs -n1 ampy --port ${PORT} mkdir
 # put medea files
-find medea -name '*.py' | grep -v auth.py | xargs -n1 -I {} ampy --port ${PORT} put {} {}
+find medea -name '*.py' | xargs -n1 -I {} ampy --port ${PORT} put {} {}
 # put script example files
 find examples -name '*.py' | xargs -n1 -I {} ampy --port ${PORT} put {} {}
 # put script data files
