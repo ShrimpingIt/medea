358ef1f288d0a05ae5ecbc17cb3a023f7e5f0450
Cefn Hoile <github.com@cefn.com>
2018-02-15 14:05:02 +0000

    Ensured wifi connection happens only on calling loop

--- examples/scripts/twitterTimelinePollFields.py
+++ examples/scripts/twitterTimelinePollFields.py
@@ -3,13 +3,13 @@ import sys
 from time import sleep
 from examples.scripts.twitterTimelineExtractFields import run
 
-if sys.platform == "esp8266" or sys.platform == "esp32":
-    from medea.auth import wifiName, wifiPass
-    from medea.wifi import connect
-    assert wifiName is not None and wifiPass is not None, "Provide wifi credentials"
-    connect(wifiName.decode('utf8'),wifiPass.decode('utf8'))
-
 def loop():
+    if sys.platform == "esp8266" or sys.platform == "esp32":
+        from medea.auth import wifiName, wifiPass
+        from medea.wifi import connect
+        assert wifiName is not None and wifiPass is not None, "Provide wifi credentials"
+        connect(wifiName.decode('utf8'), wifiPass.decode('utf8'))
+
     while True:
         run()
         gc.collect()
