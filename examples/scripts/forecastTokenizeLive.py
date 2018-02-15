53aa4c6d42808e6d830e0e3c49a1b7a8208388e4
Cefn Hoile <github.com@cefn.com>
2018-02-15 15:29:42 +0000

    TODO removed as now fixed

--- examples/scripts/forecastTokenizeLive.py
+++ examples/scripts/forecastTokenizeLive.py
@@ -2,10 +2,6 @@ from medea.util import visit
 import medea.https as https
 import medea.weather as weather
 
-# TODO handle the way that nested httpByteGenerator within httpContentByteGenerator causes
-# loss of a single byte, preventing Object from being parsed correctly. Where is the byte mismatch?
-# How come it doesn't affect the Twitter implementation
-
 def visitor(tok, val):
     print(tok, val)
 
