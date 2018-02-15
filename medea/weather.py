9c031a4163ab915f1ed2c60c4a59febc0b74cf98
Cefn Hoile <github.com@cefn.com>
2018-02-15 15:06:25 +0000

    Allow passthrough of keyword arguments to weather URL, and set cnt=8 by default to limit JSON payload size

--- medea/weather.py
+++ medea/weather.py
@@ -4,15 +4,18 @@ from medea.auth import weatherAppId
 weatherBaseUrl = "https://api.openweathermap.org/data/2.5/"
 
 
-def createUrl(apiPath, getParams):
+def createUrl(apiPath, **k):
     url = weatherBaseUrl + apiPath + "?"
-    url += "&".join(["{}={}".format(key, value) for key, value in getParams.items()])
+    url += "&".join(["{}={}".format(key, value) for key, value in k.items()])
     gc.collect()
     return url
 
 
-def createCityUrl(city="Morecambe", country="uk"):
-    return createUrl("forecast", dict(
+def createCityUrl(city="Morecambe", country="uk", **k):
+    params = dict(
         q="{},{}".format(city, country),
         appid=weatherAppId.decode('utf-8'),
-    ))
+        cnt=8, # retrieves just 8x 3-hourly records by default (respecting limit of 8192 TLS record buffer)
+    )
+    params.update(k)
+    return createUrl("forecast", **params)
