97e43ba236ec10dc7f096b5906ae4a1689098184
Cefn Hoile <github.com@cefn.com>
2019-02-21 11:23:52 +0000

    Tweet parsing promoted into medea.twitter library

--- examples/scripts/twitterTimelineExtractFields.py
+++ examples/scripts/twitterTimelineExtractFields.py
@@ -1,42 +1,15 @@
-from medea import Tokenizer
-from medea.https import createContentByteGeneratorFactory
-from medea.twitter import twitterHeaders, createTwitterTimelineUrl
+from medea.twitter import generateTweets, createTimelineTokenizer
 from examples.scripts import timeit
 
-defaultAccount = "realDonaldTrump"
-reportNames = ["id", "full_text"] # process child values having these keys
-suppressNames = ["user", "media"] # throw away all descendants of these keys (hence throwing their false-matching "id" or "full_text" entries)
-
-def generateTweets(account=None):
-    try:
-        if account is None:
-            account=defaultAccount
-        
-        twitterUrl = createTwitterTimelineUrl(account, count=1, tweet_mode="extended")
-        byteGeneratorFactory = createContentByteGeneratorFactory(twitterUrl, twitterHeaders)
-        tokenizer = Tokenizer(byteGeneratorFactory)
-
-        def fieldGeneratorFactory(name):
-            # generator for token pairs of the named JSON value
-            valueTokenSequence = tokenizer.tokenizeValue()
-            try:
-                for tok, val in valueTokenSequence: # should be a single pair e.g. 'STR', '298743097324'
-                    if name in reportNames: 
-                        return (yield name, val)
-            except StopIteration:
-                return
-
-        fieldGenerator = tokenizer.generateFromNamed(reportNames + suppressNames, fieldGeneratorFactory)
-        
-        while True:
-            _, tweetId = next(fieldGenerator)
-            _, tweetText = next(fieldGenerator)
-            yield tweetId, tweetText
-    except StopIteration:
-        return
-
 def run():
-    for tweetId, tweetText in generateTweets():
+    
+    if sys.platform == "esp8266" or sys.platform == "esp32":
+        from medea.auth import wifiName, wifiPass
+        from medea.wifi import connect
+        assert wifiName is not None and wifiPass is not None, "Provide wifi credentials"
+        connect(wifiName.decode('utf8'), wifiPass.decode('utf8'))
+    
+    for tweetId, tweetText in generateTweets(createTimelineTokenizer("realDonaldTrump")):
         print("{} : '{}'".format(tweetId, tweetText))
 
 if __name__ == "__main__":
